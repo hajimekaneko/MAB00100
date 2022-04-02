a28571f96f45ba80ceac5decb5256c95f4707e43
hajimekaneko <hajime.10600@gmail.com>
2022-04-02 15:24:56 +0900

    OK

--- config/settings.py
+++ config/settings.py
@@ -155,6 +155,7 @@ if DEBUG:
         'https://maf00100.herokuapp.com',
         'http://10.145.77.110:8080',
         'http://172.17.0.35:8080',
+        'http://172.17.0.172:8080',
     )
     CORS_ALLOW_HEADERS = default_headers + (
     'x-kbn-token',
